9a93f398fc8c832a8fe13e9f82b94ea69c08b013
Mario Luca <lcumra@gmail.com>
2015-09-21 12:53:30 +0200

    yay

--- exercises/019/solution.py
+++ exercises/019/solution.py
@@ -6,6 +6,6 @@ This is a temporary script file.
 """
 import sys
 if len(sys.argv) == 3:
-    print(sys.argv[1] + sys.argv[2])
+    print(int(sys.argv[1]) + int(sys.argv[2]))
 else:
     print("usage: python3 solution.py OP1 OP2")
