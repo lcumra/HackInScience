2aebf17d95f3f971dcf1417786e27203e9f882f9
Mario Luca <lcumra@gmail.com>
2015-09-21 18:20:43 +0200

    p

--- exercises/050/solution.py
+++ exercises/050/solution.py
@@ -4,11 +4,10 @@ Spyder Editor
 
 This is a temporary script file.
 """
-a = 0
 b = 0
 for i in range(1, 1000):
-   if i % 3 == 0:
-        b = b + i
-        if i % 5 == 0:
-            a = a + i
-print(a + b)
+    if i % 3 == 0:
+       b = b + i
+       elif i % 5 == 0:
+           b = b + i
+print(b)
