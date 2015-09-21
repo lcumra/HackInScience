ada0438c23c294c134d8c74fb8f4986d951660a1
Mario Luca <lcumra@gmail.com>
2015-09-21 18:54:43 +0200

    p

--- exercises/070/solution.py
+++ exercises/070/solution.py
@@ -5,9 +5,10 @@ Spyder Editor
 This is a temporary script file.
 """
 import string
-ciao = list(string.ascii_lowercase)
+import itertools
+#ciao = list(string.ascii_lowercase)
+ciao = list(itertools.combinations('abcdefghijklmnopqrstuvwxyz', 2))
+lungo = len(ciao)
 
-for i in range(0, 26):
-    for j in range(0, 26):
-        if i != j:
-            print(ciao[i] + ciao[j])
+for i in range(0, lungo - 1):
+    print(ciao[i][0]+ciao[i][1])
