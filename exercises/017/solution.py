b15bbac792ae9a30ca8b0249f969b8eb6612a567
Mario Luca <lcumra@gmail.com>
2015-09-21 12:46:37 +0200

    p

---
+++ exercises/017/solution.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import sys
+if len(sys.argv) == 3:
+    print(sys.argv[1] + sys.argv[2])
+else:
+    print("usage: python3 solution.py OP1 OP2")
