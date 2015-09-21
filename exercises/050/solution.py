869a9792d50aa638c656b0e19699aaf3086dc586
Mario Luca <lcumra@gmail.com>
2015-09-21 18:17:51 +0200

    p

--- exercises/050/solution.py
+++ exercises/050/solution.py
@@ -7,8 +7,8 @@ This is a temporary script file.
 a = 0
 b = 0
 for i in range(1, 1000):
-    if i % 3 == 0:
+   if i % 3 == 0:
         b = b + i
-    if i % 5 == 0:
-        a = a + i
+        if i % 5 == 0:
+            a = a + i
 print(a + b)
