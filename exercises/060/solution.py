536e9a2b6c7c4a9e1ef21d94cec4f57d51515ece
Mario Luca <lcumra@gmail.com>
2015-09-21 18:28:28 +0200

    p

---
+++ exercises/060/solution.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import string
+ciao = list(string.ascii_lowercase)
+
+for i in range(0, 26):
+    for j in range(0, 26):
+        print(ciao[i], ciao[j])
