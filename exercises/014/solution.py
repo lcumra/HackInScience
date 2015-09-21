9ae1847d475096a0d11a696082dacbdfcd21bb94
Mario Luca <lcumra@gmail.com>
2015-09-21 12:28:07 +0200

    p

---
+++ exercises/014/solution.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import sys
+ciao = sys.argv[1]
+
+if len(ciao) > 0:
+    print(ciao)
+else:
+    print("usage: python3 solution.py PARAM")
