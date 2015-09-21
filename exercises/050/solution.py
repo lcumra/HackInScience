daa5b1653ac2ac236183778f49e5505814a1e553
Mario Luca <lcumra@gmail.com>
2015-09-21 18:23:48 +0200

    p

--- exercises/050/solution.py
+++ exercises/050/solution.py
@@ -7,7 +7,7 @@ This is a temporary script file.
 b = 0
 for i in range(1, 1000):
     if i % 3 == 0:
-       b = b + i
+        b = b + i
     elif i % 5 == 0:
-       b = b + i
+        b = b + i
 print(b)
