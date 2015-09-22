6b6c954be646f3c920610025d44e634037d1046a
Mario Luca <lcumra@gmail.com>
2015-09-22 10:16:36 +0200

    p

--- exercises/085/solution.py
+++ exercises/085/solution.py
@@ -6,13 +6,16 @@ Created on Mon Sep 21 18:56:59 2015
 """
 from operator import *
 
+
 def sort_a_list(lista):
     print(sorted(lista, revrese=True))
     return;
 
+
 def sort_by_mark(mycl):
     print(sorted(mycl, key=itemgetter(1), reverse=True))
     return;
+
 
 def sort_by_name(mclass):
     print(sorted(mclass, key=itemgetter(2)))
