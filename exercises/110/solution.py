2b215e05e9187f021395468dc587d0f94b526d22
Mario Luca <lcumra@gmail.com>
2015-09-22 15:50:35 +0200

    p

--- exercises/110/solution.py
+++ exercises/110/solution.py
@@ -1,35 +1,35 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Sep 22 12:55:46 2015
+Created on Tue Sep 22 15:34:44 2015
 
 @author: pippo
 """
 import sys
 comandi = ['+', '-', '\*', '//', '%', '^']
 
-if len(sys.argv) > 3:
-    def opera(a, b, c):
-        if a.is_integer():
-            if c.is_integer():
-                if b in comandi:
-                    if b == '+':
-                        d = a + b
-                    elif b == '-':
-                        d = a - b
-                    elif b == '\*':
-                        d = a * b
-                    elif b == '//':
-                        d = a / b
-                    elif b == '%':
-                        d = a % b
-                    else:
-                        d = a ** b
-                    print(d)
-                else:
-                    print('input error')
+if len(sys.argv) == 4:
+    b = sys.argv[2]
+    try:
+        a = int(sys.argv[1])
+        c = int(sys.argv[3])
+        if b in comandi:
+            if b == '+':
+                d = a + b
+            elif b == '-':
+                d = a - b
+            elif b == '\*':
+                d = a * b
+            elif b == '//':
+                d = a / b
+            elif b == '%':
+                d = a % b
             else:
-                print('input error')
+                d = a ** b
+            print(d)
         else:
             print('input error')
+        #break
+    except:
+        print("input errore")
 else:
     print('usage: ./solution.py a_number (an_operator +-*/%^) a_number')
---
+++ exercises/110/solution1.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 12:55:46 2015
+
+@author: pippo
+"""
+import sys
+comandi = ['+', '-', '\*', '//', '%', '^']
+
+if len(sys.argv) > 3:
+    def opera(a, b, c):
+        if isinstance(a, int):
+            if isinstance(c, int):
+                if b in comandi:
+                    if b == '+':
+                        d = a + b
+                    elif b == '-':
+                        d = a - b
+                    elif b == '\*':
+                        d = a * b
+                    elif b == '//':
+                        d = a / b
+                    elif b == '%':
+                        d = a % b
+                    else:
+                        d = a ** b
+                    print(d)
+                else:
+                    print('input error')
+            else:
+                print('input error')
+        else:
+            print('input error')
+else:
+    print('usage: ./solution.py a_number (an_operator +-*/%^) a_number')
