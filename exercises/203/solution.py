5a961faaeeffb8c9ae1ebccf6a72d7b04adda550
Mario Luca <lcumra@gmail.com>
2015-09-22 16:59:49 +0200

    pollo

---
+++ exercises/203/solution.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 16:48:26 2015
+
+@author: pippo
+"""
+
+
+def is_multiple(a, b):
+    if a % b == 0:
+        return True
+    else:
+        return False
