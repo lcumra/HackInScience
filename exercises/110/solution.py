aeebda9a16011c545d0f27a55f96cdf85b50aa53
Mario Luca <lcumra@gmail.com>
2015-09-22 15:24:21 +0200

    p

--- exercises/110/solution.py
+++ exercises/110/solution.py
@@ -4,7 +4,32 @@ Created on Tue Sep 22 12:55:46 2015
 
 @author: pippo
 """
+import sys
+comandi = ['+', '-', '\*', '//', '%', '^']
 
-
-def opera(a, b, c)
-
+if len(sys.argv) > 3:
+    def opera(a, b, c):
+        if a.is_integer():
+            if c.is_integer():
+                if b in comandi:
+                    if b == '+':
+                        d = a + b
+                    elif b == '-':
+                        d = a - b
+                    elif b == '\*':
+                        d = a * b
+                    elif b == '//':
+                        d = a / b
+                    elif b == '%':
+                        d = a % b
+                    else:
+                        d = a ** b
+                    print(d)
+                else:
+                    print('input error')
+            else:
+                print('input error')
+        else:
+            print('input error')
+else:
+    print('usage: ./solution.py a_number (an_operator +-*/%^) a_number')           
