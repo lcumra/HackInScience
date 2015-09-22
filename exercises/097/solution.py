783f19d9893262e8aec9990d0d622950b0b4ad37
Mario Luca <lcumra@gmail.com>
2015-09-22 10:56:13 +0200

    p

--- exercises/097/solution.py
+++ exercises/097/solution.py
@@ -15,3 +15,17 @@ def love_meet(a, b):
             if a[i] is b[j]:
                 meet.append(a[i])
     return meet
+
+
+def affair_meet(a, b, c):
+    meet = list()
+    alen = len(a)
+    blen = len(b)
+    clen = len(c)
+    for i in range(0, alen):
+        for j in range(0, blen):
+            if a[i] is not b[j]:
+                for k in range(0, clen):
+                    if a[i] is c[k]:
+                        meet.append(a[i])
+    return meet
