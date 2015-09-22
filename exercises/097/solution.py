9fe439570632731ab55c6e0eed58d7282ebdc9b6
Mario Luca <lcumra@gmail.com>
2015-09-22 11:22:52 +0200

    p

--- exercises/097/solution.py
+++ exercises/097/solution.py
@@ -7,18 +7,11 @@ Created on Mon Sep 21 18:56:59 2015
 
 
 def love_meet(a, b):
-    meet = set()
-    alen = len(a)
-    for i in range(0, alen):
-        if a[i] in b:
-            meet.add(a[i])
+    meet = a.intersect(b)
     return meet
 
 
 def affair_meet(b, a, c):
-    meet = set()
-    alen = len(a)
-    for i in range(0, alen):
-        if a[i] in c and a[i] not in b:
-            meet.add(a[i])
-    return meet
+    meet = a.intersect(c)
+    unmeet = meet.difference(b)
+    return unmeet
