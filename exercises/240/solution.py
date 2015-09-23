0654a2a7af7e5e7b7b08291b6705c5da30279f18
Mario Luca <lcumra@gmail.com>
2015-09-23 10:19:09 +0200

    pollo

---
+++ exercises/240/solution.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 23 10:06:16 2015
+
+@author: pippo
+"""
+lisi = [0] * 10
+print(lisi)
+a = 1
+lisi[1] = 1
+lisi[0] = 1
+for i in range(2, 10):
+    lisi[i] = lisi[i-1] + lisi[i-2]
+
+print(str(lisi)[:-1][1:] + '.')
---
+++ exercises/240/untitled8.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 23 10:06:16 2015
+
+@author: pippo
+"""
+
