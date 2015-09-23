31cb79e0b288049eb2fc6c60bb3a3a8e0d45a709
Mario Luca <lcumra@gmail.com>
2015-09-23 10:39:49 +0200

    pollo

--- exercises/220/solution.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Sep 23 10:24:54 2015
-
-@author: pippo
-"""
-
-print('10007, 10009, 10037, 10039')
--- exercises/285/solution.py
+++ exercises/285/solution.py
@@ -9,6 +9,7 @@ import requests
 
 try:
     r = requests.get('http://mdk.fr/ip')
-    print(r)
+    testo = r.text.split('\n')
+    print(testo[0])
 except ConnectionError as conny:
     print('No internet connectivity.')
