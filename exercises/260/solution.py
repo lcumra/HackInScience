254adadc867e6ac5e80f96ba3924c28f6c4d248a
Mario Luca <lcumra@gmail.com>
2015-09-24 13:56:17 +0200

    yay

---
+++ exercises/260/solution.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 24 13:55:18 2015
+
+@author: pippo
+"""
+
+import math
+import numpy
+
+
+def euclidean(a, b):
+    d = ((b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2) ** 0.5
+    return d
+
+
+def opt_euclidean(a, b):
+    d2 = math.pow(math.pow(b[0] - a[0], 2) + math.pow(b[1] - a[1], 2), 0.5)
+    return d2
+
+
+def np_euclidean(a, b):
+    d3 = numpy.power(numpy.power(b[0] - a[0], 2) +
+                     numpy.power(b[0] - a[0], 2), 0.5)
+    return d3
