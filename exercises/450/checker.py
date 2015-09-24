6d78a27b135d0c896e1cecf01455b36274a3c949
Mario Luca <lcumra@gmail.com>
2015-09-24 16:34:41 +0200

    yay

--- exercises/450/checker.py
+++ exercises/450/checker.py
@@ -6,11 +6,15 @@ Created on Thu Sep 24 15:15:50 2015
 """
 import string
 al = list(string.ascii_lowercase)
+AL = list(string.ascii_uppercase)
 
 
 def numa(x):
     for i in range(0, len(al)):
         if x == al[i]:
             return i
+    for i in range(0, len(al)):
+        if x == AL[i]:
+            return i + 0.5
     else:
         return 666
--- exercises/450/solution.py
+++ exercises/450/solution.py
@@ -7,6 +7,7 @@ Created on Thu Sep 24 15:09:48 2015
 import string
 import checker
 al = list(string.ascii_lowercase)
+AL = list(string.ascii_uppercase)
 
 
 def caesar_cypher(s, k, m):
@@ -15,12 +16,20 @@ def caesar_cypher(s, k, m):
     resu = list(s)
     for i in range(0, len(s)):
         if checker.numa(ori[i]) != 666:
-            code[i] = checker.numa(ori[i])
-            if m == "backward":
-                code[i] = (code[i] - k) % 26
-            if m == "forward":
-                code[i] = (code[i] + k) % 26
-                resu[i] = al[code[i]]
+            if checker.numa(ori[i]) % 1 == 0:
+                code[i] = checker.numa(ori[i])
+                if m == "backward":
+                    code[i] = (code[i] - k) % 26
+                if m == "forward":
+                    code[i] = (code[i] + k) % 26
+                    resu[i] = al[code[i]]
+            if checker.numa(ori[i]) % 1 == 0.5:
+                code[i] = int(checker.numa(ori[i]) - 0.5)
+                if m == "backward":
+                    code[i] = (code[i] - k) % 26
+                if m == "forward":
+                    code[i] = (code[i] + k) % 26
+                    resu[i] = AL[code[i]]
         else:
             resu[i] = ori[i]
     return ''.join(resu)
